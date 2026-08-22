87987b2517b6dacddc92cd164700e89476cd73a0
Kambala Kalyan Reddy <kalyanreddy895@gmail.com>
2026-08-22 21:56:11 +0530

    Time: 2 ms (91.97%), Space: 47.5 MB (93.57%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int low = 0;
+        
+        for(int i = 0 ; i<nums.length ; i++){
+            if(nums[i] != 0){
+                int temp = nums[low];
+                nums[low] = nums[i];
+                nums[i] = temp;
+                low++;
+               
+            }
+           
+        }
+        
+        
+    }
+}
