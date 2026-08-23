d4e512a78570fcae84cc3027e0af10b0097448d9
Kambala Kalyan Reddy <kalyanreddy895@gmail.com>
2026-08-23 21:49:34 +0530

    Time: 15 ms (24.47%), Space: 52.3 MB (95.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       HashMap<Integer,Integer> map = new HashMap<>();
+		for(int i = 0 ; i<nums.length ; i++) {
+			if(map.containsKey(nums[i])) {
+				map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+			
+			}
+			else {
+				map.put(nums[i], 1);
+			}
+		}
+		int max =  0;
+		int maxe = 0;
+		for(int i = 0 ; i<nums.length ; i++) {
+			if(map.get(nums[i]) > max) {
+                max = map.get(nums[i]);
+				maxe = nums[i];
+			}
+			
+		
+		}
+		return maxe;
+    }
+}
